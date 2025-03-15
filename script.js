a1852d7aa919681667f96d6d1c29e9d880553aa6
NicolasBroyad <113180571+NicolasBroyad@users.noreply.github.com>
2025-03-15 20:40:57 -0300

    Full excercise

---
+++ script.js
@@ -0,0 +1,86 @@
+// console.log(Math.random());
+
+function getComputerChoice() {
+    let randomInt = Math.random();
+    let computerChoice = "";
+
+    if(randomInt <= 0.333333) {
+        computerChoice = "Rock";
+    } else if(randomInt > 0.333333 && randomInt <= 0.666666) {
+        computerChoice = "Paper";
+    } else if(randomInt > 0.6666666) {
+        computerChoice = "Scissors";
+    }
+
+    return computerChoice;
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Enter your choice ('Rock'/'Paper'/'Scissors')");
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+
+    return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    let winner = "No winner";
+    if(humanChoice == "Rock" && computerChoice == "Paper"){
+        winner = "Computer";
+        console.log("You lose! Paper beats Rock");
+    } else if(humanChoice == "Rock" && computerChoice == "Scissors"){
+        winner = "Human";
+        console.log("You won! Rock beats Scissors");
+    } else if(humanChoice == "Paper" && computerChoice == "Rock"){
+        winner = "Human";
+        console.log("You won! Paper beats Rock");
+    } else if(humanChoice == "Paper" && computerChoice == "Scissors") {
+        winner = "Computer";
+        console.log("You lose! Scissors beats Paper");
+    } else if(humanChoice == "Scissors" && computerChoice == "Rock") {
+        winner = "Computer";
+        console.log("You lose! Rock beats Scissors");
+    } else if(humanChoice == "Scissors" && computerChoice == "Paper"){
+        winner = "Human";
+        console.log("You won! Scissors beats Paper");
+    } else {
+        console.log("There was a tie ");
+    }
+
+    if(winner != "No winner") {
+        console.log(winner + " wins this round!");
+    } else {
+        console.log("No winner this round");
+    }
+
+    return winner;
+}
+
+function playGame() {
+    let roundWinner;
+    for(i=0; i < 5; i++){
+        console.log("Round " + parseInt(i+1) + " out of 5");
+        roundWinner = playRound(getHumanChoice(),getComputerChoice());
+
+        if(roundWinner == "Human"){
+            humanScore++;
+        } else if(roundWinner == "Computer"){
+            computerScore++;
+        }
+        console.log("Human score: " + humanScore + " // Computer score: " + computerScore);
+    }
+
+    if(humanScore > computerScore){
+        console.log("Human wins the game");
+    } else if(computerScore > humanScore){
+        console.log("Computer wins the game");
+    } else {
+        console.log("The match ended with a tie");
+    }
+}
+
+// const humanSelection = getHumanChoice();
+// const computerSelection = getHumanChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+playGame();
